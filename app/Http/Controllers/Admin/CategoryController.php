8b9bf2841e1b1e21e98ddaaf0b580518d57e626d
Yoedi Arianto <yoediarianto@gmail.com>
2024-07-17 14:34:37 +0700

    Create process of delete category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -119,6 +119,9 @@ class CategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $item = Category::findOrFail($id);
+        $item->delete();
+
+        return redirect()->route('category.index');
     }
 }
