bf694ccfcf7cd59f34247e2cfa01d43aaaeb4ce1
Yoedi Arianto <yoediarianto@gmail.com>
2024-07-26 08:13:21 +0700

    Integrate cart badge

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -76,8 +76,16 @@
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link d-inline-block mt-2">
-                        <img src="/images/icon-cart-empty.svg" alt="" />
-                </a>
+                        @php
+                            $carts = \App\Models\Cart::where('user_id', Auth::user()->id)->count();
+                        @endphp
+                        @if ($carts > 0)
+                            <img src="/images/icon-cart-filled.svg" alt="" />
+                            <div class="card-badge">{{$carts}}</div>
+                        @else
+                            <img src="/images/icon-cart-empty.svg" alt="" />
+                        @endif
+                    </a>
                 </li>
             </ul>
 
