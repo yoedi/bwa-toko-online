018b06c4423bc260e8ef26e5198e4988d096157a
Yoedi Arianto <yoediarianto@gmail.com>
2024-07-30 14:21:22 +0700

    Create transaction detail model

---
+++ app/Models/TransactionDetail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionDetail extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'transaction_id',
+        'product_id',
+        'price',
+        'shipping_status',
+        'resi',
+        'code'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+}
