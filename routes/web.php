d250761e7af2cff890b61a1ea628f5392f97323e
Yoedi Arianto <yoediarianto@gmail.com>
2024-08-01 21:15:45 +0700

    Add auth middleware to specific route

--- routes/web.php
+++ routes/web.php
@@ -20,32 +20,40 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
 Route::get('/categories', [CategoryController::class, 'index'])->name('category');
 Route::get('/categories/{id}', [CategoryController::class, 'detail'])->name('category-detail');
+
 Route::get('/details/{id}', [DetailController::class, 'index'])->name('detail');
 Route::post('/details/{id}', [DetailController::class, 'add'])->name('detail-add');
-Route::get('/cart', [CartController::class, 'index'])->name('cart');
-Route::delete('/cart/{id}', [CartController::class, 'delete'])->name('cart-delete');
 
-Route::post('/checkout', [CheckoutController::class, 'process'])->name('checkout');
 Route::get('/checkout/callback', [CheckoutController::class, 'callback'])->name('midtrans-callback');
+
 Route::get('/success', [CartController::class, 'success'])->name('success');
 
 Route::get('/register/success', [RegisteredUserController::class, 'success'])->name('register-success');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/dashboard/products', [DashboardProductController::class, 'index'])->name('dashboard-product');
-Route::get('/dashboard/product/details/{id}', [DashboardProductController::class, 'detail'])->name('dashboard-product-detail');
-Route::get('/dashboard/product/create', [DashboardProductController::class, 'create'])->name('dashboard-product-create');
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/cart', [CartController::class, 'index'])->name('cart');
+    Route::delete('/cart/{id}', [CartController::class, 'delete'])->name('cart-delete');
 
-Route::get('/dashboard/transactions', [DashboardTransactionController::class, 'transactions'])->name('dashboard-transactions');
-Route::get('/dashboard/transactions/{id}', [DashboardTransactionController::class, 'details'])->name('dashboard-transaction-deteails');
+    Route::post('/checkout', [CheckoutController::class, 'process'])->name('checkout');
 
-Route::get('/dashboard/setting', [DashboardSettingController::class, 'store'])->name('dashboard-setting-store');
-Route::get('/dashboard/account', [DashboardSettingController::class, 'account'])->name('dashboard-setting-account');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard/products', [DashboardProductController::class, 'index'])->name('dashboard-product');
+    Route::get('/dashboard/product/details/{id}', [DashboardProductController::class, 'detail'])->name('dashboard-product-detail');
+    Route::get('/dashboard/product/create', [DashboardProductController::class, 'create'])->name('dashboard-product-create');
+
+    Route::get('/dashboard/transactions', [DashboardTransactionController::class, 'transactions'])->name('dashboard-transactions');
+    Route::get('/dashboard/transactions/{id}', [DashboardTransactionController::class, 'details'])->name('dashboard-transaction-deteails');
+
+    Route::get('/dashboard/setting', [DashboardSettingController::class, 'store'])->name('dashboard-setting-store');
+    Route::get('/dashboard/account', [DashboardSettingController::class, 'account'])->name('dashboard-setting-account');
+});
 
 Route::prefix('admin')
     // ->namespace('Admin')
+    ->middleware(['auth', 'admin'])
     ->group(function() {
         Route::get('/', [AdminDashboardController::class, 'index'])->name('admin-dashboard');
         Route::get('/category/list', [AdminCategoryController::class, 'list'])->name('list-category');
